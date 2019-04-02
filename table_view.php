f432a3d10e01bb5c788736befbd8016a57aeb3b5
angelofernando98 <angelo.fernando2pac@gmail.com>
2019-04-02 13:43:24 +0000

    Create table_view.php

---
+++ table_view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Input</title>
+</head>
+<body>
+
+<form action="../Input/dbi" method="post">
+  First name:<br>
+  <input type="text" name="name">
+  <br>
+  Last name:<br>
+  <input type="text" name="sur">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+
+</body>
+</html>
